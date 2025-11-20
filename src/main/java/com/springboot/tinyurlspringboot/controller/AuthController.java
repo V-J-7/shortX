06e51ee22fc478595fa32722808bd05a34d5d1ed
V-J-7 <vatsaljain231@gmail.com>
2025-11-20 20:11:32 +0530

    made ui more responsive

--- src/main/java/com/springboot/tinyurlspringboot/controller/AuthController.java
+++ src/main/java/com/springboot/tinyurlspringboot/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.springboot.tinyurlspringboot.services.LoginService;
 import com.springboot.tinyurlspringboot.services.UserAuthService;
 import com.springboot.tinyurlspringboot.services.UserService;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +25,19 @@ public class AuthController {
     }
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody SignupDTO signupDTO) {
-
+    // 1. Keep this for good UX (fast fail)
         if (userService.userExists(signupDTO.getEmail())){
             return ResponseEntity.status(401).body("Already Registered");
         }
-        userService.addUser(signupDTO.getEmail(), signupDTO.getPassword());
-        return ResponseEntity.ok("Registered Successfully");
+
+        try {
+            // 2. Try to save
+            userService.addUser(signupDTO.getEmail(), signupDTO.getPassword());
+            return ResponseEntity.ok("Registered Successfully");
+        
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(401).body("Already Registered");
+        }
     }
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
